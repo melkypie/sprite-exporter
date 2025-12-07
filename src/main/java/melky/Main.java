5b51c97b68eea28e3029ca1aa3d66a990dfa1ab2
Ron Young <ron@ryoung.io>
2025-12-07 16:19:20 -0600

    log error and sysexit 1

--- src/main/java/melky/Main.java
+++ src/main/java/melky/Main.java
@@ -9,18 +9,28 @@ public class Main
 {
 	public static void main(String[] args) throws IOException
 	{
-		var cacheDownloader = new CacheDownloader(Env.OLD_CACHE, Env.NEW_CACHE);
-		var cachePath = Path.of(Env.CACHE_FOLDER);
 
-		// todo: maybe generate sprite diffs later
-		var archive = cacheDownloader.downloadRevision(cachePath, cacheDownloader.getNewCache());
-		var extractFolder = cachePath.resolve(cacheDownloader.getNewCache());
-		Archive.untar(archive, extractFolder);
+		try
+		{
 
-		Path spritePath = extractFolder.resolve(Env.SPRITE_FOLDER);
-		SpriteDumper.dump(extractFolder, spritePath);
+			var cacheDownloader = new CacheDownloader(Env.OLD_CACHE, Env.NEW_CACHE);
+			var cachePath = Path.of(Env.CACHE_FOLDER);
 
-		SampleGenerator.generate(spritePath, Path.of(Env.PACK_FOLDER));
+			// todo: maybe generate sprite diffs later
+			var archive = cacheDownloader.downloadRevision(cachePath, cacheDownloader.getNewCache());
+			var extractFolder = cachePath.resolve(cacheDownloader.getNewCache());
+			Archive.untar(archive, extractFolder);
+
+			Path spritePath = extractFolder.resolve(Env.SPRITE_FOLDER);
+			SpriteDumper.dump(extractFolder, spritePath);
+
+			SampleGenerator.generate(spritePath, Path.of(Env.PACK_FOLDER));
+		}
+		catch (Throwable e)
+		{
+			log.error("{}", e);
+			System.exit(1);
+		}
 
 		// okhttp3 connection pool hangs after http2 requests without Exit signal
 		System.exit(0);
